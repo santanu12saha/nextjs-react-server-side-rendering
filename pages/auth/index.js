63cc3360fb3dfa38ba01ec226f947a645c20495f
santanu12saha <saha.santanu0217@gmail.com>
2020-03-14 23:08:18 +0530

    Added css styling using <style jsx></style> in next js.

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -6,6 +6,14 @@ const authIndexPage = () => (
     <div>
         <h1>The Auth Index Page</h1>
         <User name="Santanu" age={30} />
+        <style jsx>{`
+            div {
+                border: 1px solid #eee;
+                box-shadow: 0 2px 3px #ccc;
+                padding: 20px;
+                text-align: center;
+            }
+        `}</style>
     </div>
 );
 
